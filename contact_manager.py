695ac77d06dbc1282c3f461f6ddecd1b43e93a53
Allan Mwesigwa <Allan Mwesigwa>
2016-08-25 20:58:00 +0300

    Fix Bug the sends messages to duplicate contacts
    The error of the send message function sending messages to duplicate
    contacts in the database is fixed
    
    ref #9

--- contact_manager.py
+++ contact_manager.py
@@ -10,7 +10,7 @@ class ContactManager(object):
 		conn = sqlite3.connect('CM.db')
 		conn.execute('''CREATE TABLE IF NOT EXISTS CONTACTS (
 		ID INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, 
-		NAME TEXT NOT NULL, 
+		NAME TEXT NOT NULL,
 		PHONE_NUM INT NOT NULL) 
 		''')
 
@@ -31,20 +31,85 @@ class ContactManager(object):
 	def search_record(self, name):
 		conn = sqlite3.connect('CM.db')
 		cursor = conn.execute("SELECT * from CONTACTS WHERE NAME='{}'" .format(name));
+		
 		data = cursor.fetchall()
 
 		if not len(data):
 			return 'No contact found with name %s' %name
-		for row in cursor:
-			return ('{} \t{}' .format(row[1], row[2]))
+		elif len(data) > 1:
+			print "Which %s" %name, "?" 
+
+			results = []
+			for row in data:
+				each_row = []
+				
+				each_row.append(row[0])
+				each_row.append(row[1])
+				each_row.append(row[2])
+				results.append(each_row)
+
+			print tabulate(results, headers=["ID", "Name", "Number"])
+
+			choice = input("\nPlease type the number corresponding to the person")
+
+			if choice > len(data)+1 or choice <= 0:
+				return 'Invalid selection'
+			else:
+				new_cursor = conn.execute("SELECT NAME from CONTACTS WHERE ID='{}'" .format(choice))
+				theNumber = new_cursor.fetchall()
+				return theNumber[0][0]
+
+		else:
+
+			results = []
+			for row in data:
+				each_row = []
+				
+				each_row.append(row[1])
+				each_row.append(row[2])
+				results.append(each_row)
+
+			return tabulate(results, headers=["Name", "Number"])
 
 		conn.close()
 
-	def get_number(name):
-		cursor = conn.execute("SELECT PHONE_NUM from CONTACTS WHERE NAME='{}'" .format(name)); 		
+def send_text(name, message):
+	conn = sqlite3.connect('CM.db')
+	cursor = conn.execute("SELECT * from CONTACTS WHERE NAME='{}'" .format(name)); 		
 
-		if cursor != None:
-			for row in cursor:
-				return ('{}' .format(row[0]))
+	data = cursor.fetchall()
+
+	if not len(data):
+		return '\nPlease add the contact before you sms'
+	elif len(data) > 1:
+		print "Which %s" %name, "would you love/like to text?" 
+
+		results = []
+		for row in data:
+			each_row = []
+			
+			each_row.append(row[0])
+			each_row.append(row[1])
+			each_row.append(row[2])
+			results.append(each_row)
+
+		print tabulate(results, headers=["ID", "Name", "Number"])
+
+		choice = input("\nPlease type the number corresponding to the person")
+
+		if choice > len(data)+1 or choice <= 0:
+			return 'Invalid selection'
 		else:
-			return 'No records found'
+			new_cursor = conn.execute("SELECT PHONE_NUM from CONTACTS WHERE ID='{}'" .format(choice))
+
+			theNumber = new_cursor.fetchall()
+			return "\nSending message to %s %d" %(name,theNumber[0][0]) + "..."
+
+			#send_message(theNumber, message)
+
+
+	else:
+		to_number = data[0][2]
+		return "\nSending message to %s %d" %(name, to_number)
+
+		#send_message(to_number, message)
